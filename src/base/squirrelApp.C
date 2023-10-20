d2c43d152825a2cec78843542bbc0cec421862ff
pjfpf <pjfpf@dtu.dk>
2023-10-20 13:31:47 +0200

    Initial files

---
+++ src/base/squirrelApp.C
@@ -0,0 +1,50 @@
+#include "squirrelApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+squirrelApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  return params;
+}
+
+squirrelApp::squirrelApp(InputParameters parameters) : MooseApp(parameters)
+{
+  squirrelApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+squirrelApp::~squirrelApp() {}
+
+void 
+squirrelApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ModulesApp::registerAllObjects<squirrelApp>(f, af, s);
+  Registry::registerObjectsTo(f, {"squirrelApp"});
+  Registry::registerActionsTo(af, {"squirrelApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+squirrelApp::registerApps()
+{
+  registerApp(squirrelApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+squirrelApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  squirrelApp::registerAll(f, af, s);
+}
+extern "C" void
+squirrelApp__registerApps()
+{
+  squirrelApp::registerApps();
+}
---
+++ test/src/base/squirrelTestApp.C
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "squirrelTestApp.h"
+#include "squirrelApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+
+InputParameters
+squirrelTestApp::validParams()
+{
+  InputParameters params = squirrelApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  return params;
+}
+
+squirrelTestApp::squirrelTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  squirrelTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+squirrelTestApp::~squirrelTestApp() {}
+
+void
+squirrelTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  squirrelApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"squirrelTestApp"});
+    Registry::registerActionsTo(af, {"squirrelTestApp"});
+  }
+}
+
+void
+squirrelTestApp::registerApps()
+{
+  registerApp(squirrelApp);
+  registerApp(squirrelTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+squirrelTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  squirrelTestApp::registerAll(f, af, s);
+}
+extern "C" void
+squirrelTestApp__registerApps()
+{
+  squirrelTestApp::registerApps();
+}
